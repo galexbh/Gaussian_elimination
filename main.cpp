5dd16c5da2e1f61752960bb945389ff846b628b5
Gabriel Barrientos <37647434+VanhelsingX3@users.noreply.github.com>
2018-11-23 23:24:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,56 @@
 /**Gaussian elimination programming class I C ++**/
 #include <iostream>
-#include <cmath>
+#include <time.h>
+#include <math.h>
+#include <cstdlib>
 #include <iomanip>
 using namespace std;
+char jump= '\n';
 
 int main()
 {
-    cout << "ingrese los coeficientes de la matriz A" << endl;
-    return 0;
-}
+    srand(time(NULL));
+
+    int N=4;
+
+    float X[N][N];
+    int Y[10];
+
+    for(int i=0; i < N; i++){
+        for(int j=0; j< N; j++ ){
+        X[i][j] = rand()%9+1;
+        }
+
+    }
+
+    for(int i=0; i < N; i++){
+        for(int j=0; j< N; j++ ){
+                cout << setw(3);
+        cout <<" "<< X[i][j];
+    if (j==N-1){
+        cout<<jump;
+    }
+        }
+
+    }
+
+    cout<<"---------------"<<jump;
+         if (X[0][0]!=1){
+                float F= X[0][0];
+            for(int k=0; k < N; k++){
+                    X[0][k] = (X[0][k])/(F);
+                    cout<<"<--------------->"<<jump;
+            }
+                }
+
+     for(int i=0; i < N; i++){
+        for(int j=0; j< N; j++ ){
+                cout << setw(3);
+
+        cout <<" "<< X[i][j];
+    if (j==N-1){
+        cout<<jump;
+    }
+        }
+
+    }
