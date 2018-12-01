790fda946fa85a15abd1a022ce895d5455b3f4c0
Gabriel Barrientos <37647434+VanhelsingX3@users.noreply.github.com>
2018-11-30 22:49:38 -0600

    Update main.cpp

--- Contribucion de edwin/RealizadoCerosALosElementos/main.cpp
+++ Contribucion de edwin/RealizadoCerosALosElementos/main.cpp
@@ -1,125 +1,136 @@
+/**Gaussian elimination programming class I C ++**/
+
 #include <iostream>
-#include <time.h>
-#include <math.h>
+#include <ctime>
+#include <cmath>
 #include <cstdlib>
 #include <iomanip>
-using namespace std;
 
+/*Declaración de funciones*/
+
+//float BuscarMultiplo(int b,float X[][4],int M, int N);
+
+/* ----------------------------------------- */
+
+using namespace std;
 int main()
 {
-    int N= 3;
-//int N= rand()%5+2;
-    float BuscarMultiplo(int b,float X[][4],int M, int N);
-    int a = 1;
-    int b= 0;
-    float aux=0;
-    float eliminar = 0;
-    int diagonal=1;
-    int estatico=0;
-    srand(time(NULL));
 
+    /**Declaración de las variables**/
 
-    float multiplo, variado, division;
+    int N = 3;            /* Tamaño de la matriz*/
+//    int N= rand()%5+2; /* Números al azar pruebas de funcionamiento*/
+    int a = 1, b = 0, diagonal=1, estatico=0;
     int fijo, recorrido;
+    float aux = 0, eliminar = 0;
+    float multiplo, variado, division;
+
+    srand(time(NULL));  /*Función para generar números al azar con tiempo de la BIOS*/
 
     //Matriz
-    float X[4][4];
-    float temp[4][4];
-    // int B[100];
+    float X[100][100];
+    float temp[100][100];
+//    int B[100];
 
-    /**Ingreso de datos**/
-    
-    for(int i=0; i < N; i++)
-    {
-        for(int j=0; j< N; j++ )
-        {            
-                cout<<"**************************--------------------> ";
-                X[i][j] = rand()%4+1;
-                temp[i][j] = X[i][j];
+    /** Entrada de datos **/
+
+    /*cout << "Ingrese el tamaño de la matriz" <<endl;
+    cin >> N;*/
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+
+//            X[i][j] = rand()%4+1;       /* Números al azar por pruebas*/
+            cout << "[" << i << "," << j <<"] = ";
+            cin >> X[i][j];
+            temp[i][j] = X[i][j];
         }
     }
-/*-------------------------*/
-    cout<<"Imprimiendo la  matriz original"<<endl;
-    for(int i=0; i < N; i++)
-    {
-        for(int j=0; j< N; j++ )
-        {
+
+    /* ----------------------------------------- */
+
+    /** Imprimir matriz **/
+
+    cout << "Imprimiendo la  matriz original" << endl;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++ ){
+
             cout << setw(3);
             cout <<" "<< X[i][j];
-            if (j==N-1)
-            {
-                cout<<endl;
+            if (j==N-1){
+
+                cout << endl;
             }
         }
-
     }
+    /* ----------------------------------------- */
 
+    /** Función **/
 
-
-    cout<<"---------------"<<endl;
+    cout << "---------------" << endl;
     //Hacer toda la diagonal 1. Crear una funcion para este ciclo
-    for (int D=1; D<N; D++)
-    {
+    for (int D=1; D<N; D++){
+
         diagonal = X[D][D];
-        for (int M=0; M<N; M++)
-        {
+
+        for (int M=0; M<N; M++){
+
             X[D][M] = (X[D][M])/(diagonal);
         }
-
     }
- //Haciedo uno (1) al primer elemento
-    if (X[0][0]!=1)
-    {
-        float F= X[0][0];
-        for(int k=0; k < N; k++)
-        {
+//Haciedo uno (1) al primer elemento
+
+    if (X[0][0]!=1){
+
+        float F = X[0][0];
+        for(int k=0; k < N; k++){
+
             X[0][k] = (X[0][k])/(F);
-          //  cout<<"<--------------->"<<endl;
+            //  cout<<"<--------------->"<<endl;
         }
     }
-    cout<<"Imprimiendo la  matriz Con la diagonal en (1)"<<endl;
-    for(int i=0; i < N; i++)
-    {
-        for(int j=0; j< N; j++ )
-        {
+
+    cout << "Imprimiendo la  matriz con la diagonal en (1)" << endl;
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+
             cout << setw(3);
             cout <<" "<< X[i][j];
-            if (j==N-1)
-            {
-                cout<<endl;
+            if (j==N-1){
+
+                cout << endl;
+
             }
         }
-
     }
 
     //Verificando si hay Ceros (abajo del 1)
 
-    do
-    {
-        for (int M=a; M<N; M++)
-        {
-            cout<<"< Haciedo Cero a los elementos. Iteraccion #>"<<M<<endl;
+    do{
+        for (int M=a; M<N; M++){
+
+            cout << "< Haciedo Cero a los elementos. Interaccion #>" << M << endl;
             estatico += b;
 
-            if(X[M][b]!=0)
-            {
+            if(X[M][b]!=0){
+
                 //buscando el multiplo
                 //aux = BuscarMultiplo(b,X,M,N);
 
-                for(int L=b; L<N; L++)
-                {
-            cout<<"El valor de M es:"<<M<<endl;
-            cout<<"El valor de estatico es: "<<estatico<<endl;
-            cout<<"El valor de L es: "<<L<<endl;
-            cout<<"El valor de X[estatico][L] es: "<<X[estatico][L]<<endl;
-            cout<<"El valor de -1*X[M][b] es: "<<(-1)*(X[M][b])<<endl;
-            cout<<"El valor de B es: "<<b<<endl;
+                for(int L=b; L<N; L++){
+
+                    cout<<"El valor de M es:"<<M<<endl;
+                    cout<<"El valor de estatico es: "<<estatico<<endl;
+                    cout<<"El valor de L es: "<<L<<endl;
+                    cout<<"El valor de X[estatico][L] es: "<<X[estatico][L]<<endl;
+                    cout<<"El valor de -1*X[M][b] es: "<<(-1)*(X[M][b])<<endl;
+                    cout<<"El valor de B es: "<<b<<endl;
                     eliminar = (X[estatico][L])*(-1*X[M][b]);// 3 * (-1*5)= -15
 
-            cout<<"El valor de ELIMIAR es: "<<eliminar<<endl;
+                    cout<<"El valor de ELIMIAR es: "<<eliminar<<endl;
                     //eliminar = -1*X[M][b];
                     X[M][L] = X[M][L] + eliminar; //X = 5+(-5) = 0
-
 
                 }
             }
@@ -127,26 +138,25 @@ int main()
         }
         a+=1;
 
-        cout<<b<<endl;
+        cout << b << endl;
     }
     while(a<N);
 
 
 
-    cout<<"Imprimiendo la nueva matriz"<<endl;
-    for(int i=0; i < N; i++)
-    {
-        for(int j=0; j< N; j++ )
-        {
-            cout << setw(3);
+    cout << "Imprimiendo la nueva matriz" << endl;
 
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+
+            cout << setw(3);
             cout <<" "<<setprecision(1)<< X[i][j];
-            if (j==N-1)
-            {
-                cout<<endl;
+
+            if (j==N-1){
+
+                cout << endl;
             }
         }
-
     }
 
     return 0;
