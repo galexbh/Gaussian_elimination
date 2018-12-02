538ac40d804e90dca04150785d60983c4882fd26
Gabriel Barrientos <37647434+VanhelsingX3@users.noreply.github.com>
2018-12-02 02:37:19 -0600

    Update main.cpp

--- Project_class_Gaussian_elimination/main.cpp
+++ Project_class_Gaussian_elimination/main.cpp
@@ -1,4 +1,4 @@
-/**Gaussian elimination programming class I C ++**/
+/** Gaussian elimination programming class I C ++   **/
 
 #include <iostream>
 #include <ctime>
@@ -6,158 +6,159 @@
 #include <cstdlib>
 #include <iomanip>
 
-/*Declaración de funciones*/
-
-//float BuscarMultiplo(int b,float X[][4],int M, int N);
-
-/* ----------------------------------------- */
-
 using namespace std;
-int main()
-{
 
-    /**Declaración de las variables**/
+/* Funciones*/
 
-    int N = 3;            /* Tamaño de la matriz*/
-//    int N= rand()%5+2; /* Números al azar pruebas de funcionamiento*/
-    int a = 1, b = 0, diagonal=1, estatico=0;
-    int fijo, recorrido;
-    float aux = 0, eliminar = 0;
+void DiagonalEnUno(int, float X[][100], float);
+void Ingreso_de_datos (float X[][100], float temp[][100], int);
+
+/*-------------------------*/
+
+int main(){
+
+    //Variables
+    int NxN=4;
+    srand(time(NULL));      //FunciÃ³n para generar nÃºmeros al azar.
+    //int NxN= rand()%10+2;
+    int a=1;
+    int C=0;
+    int b=0;
+    int Finalizar_Ciclo=1;
+    float aux=0;
+    float eliminar=0;
+    float elemento1=0, elemento2=0;
+    float diagonal=1;
+    int estatico=0;
+    int columnas_fijas=0;
+    int filas_fijas=0;
+    float elemento_Permanente1, elemento_Permanente2;
+    float Renglon_Evaluar=0;
     float multiplo, variado, division;
+    int fijo, recorrido;
+    /*-------------------------*/
 
-    srand(time(NULL));  /*Función para generar números al azar con tiempo de la BIOS*/
-
-    //Matriz
+    //Matrices
     float X[100][100];
     float temp[100][100];
-//    int B[100];
+    int Argumentos[100];
+
+    /*-------------------------*/
 
     /** Entrada de datos **/
 
-    /*cout << "Ingrese el tamaño de la matriz" <<endl;
-    cin >> N;*/
+    /*cout << "Ingrese el tamaÃ±o de la matriz" <<endl;
+    cin >> NxN;*/
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    Ingreso_de_datos(X,temp,NxN);
 
-//            X[i][j] = rand()%4+1;       /* Números al azar por pruebas*/
-            cout << "[" << i << "," << j <<"] = ";
-            cin >> X[i][j];
-            temp[i][j] = X[i][j];
-        }
-    }
+    cout<<"Imprimiendo la  matriz original"<<endl;
 
-    /* ----------------------------------------- */
+    for(int i=0; i < NxN; i++){
+        for(int j=0; j< NxN; j++ ){
 
-    /** Imprimir matriz **/
-
-    cout << "Imprimiendo la  matriz original" << endl;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++ ){
-
-            cout << setw(3);
+            cout << setw(6);
             cout <<" "<< X[i][j];
-            if (j==N-1){
-
-                cout << endl;
+            temp[i][j] = X[i][j];
+            if (j==NxN-1){
+                cout<<endl;
             }
         }
     }
-    /* ----------------------------------------- */
 
-    /** Función **/
 
-    cout << "---------------" << endl;
+    cout<<"---------------"<<endl;
     //Hacer toda la diagonal 1. Crear una funcion para este ciclo
-    for (int D=1; D<N; D++){
 
-        diagonal = X[D][D];
 
-        for (int M=0; M<N; M++){
-
-            X[D][M] = (X[D][M])/(diagonal);
-        }
-    }
 //Haciedo uno (1) al primer elemento
-
-    if (X[0][0]!=1){
-
-        float F = X[0][0];
-        for(int k=0; k < N; k++){
-
+    if (X[0][0]!=1)
+    {
+        float F= X[0][0];
+        for(int k=0; k < NxN; k++)
+        {
             X[0][k] = (X[0][k])/(F);
             //  cout<<"<--------------->"<<endl;
         }
     }
 
-    cout << "Imprimiendo la  matriz con la diagonal en (1)" << endl;
+    //Hacer ceros debajo de la primera columna
+    for(int Ceros=0; Ceros<NxN; Ceros++)
+    {
+        for(int Fila=a; Fila<NxN; Fila++)
+        {
+            elemento_Permanente2 = (-1)*(X[Fila][Ceros]);
+            for(int Columnas=C; Columnas<NxN; Columnas++)
+            {
+                elemento_Permanente1 = X[Ceros][Columnas];
+                Renglon_Evaluar = elemento_Permanente2;
+                eliminar = elemento_Permanente1*Renglon_Evaluar;
+                X[Fila][Columnas]= X[Fila][Columnas] + eliminar;
+                //cout<<"--------------------> "<<Columnas<<endl;
+            }
+            DiagonalEnUno(NxN, X, diagonal);
+        }
 
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+        a+=1;
+        C+=1;
+    }
+
+    /** Mostrar proceso de matriz con diagonal en (1)**/
+/*    cout<<"Imprimiendo la  matriz Con la diagonal en (1)"<<endl;
+    for(int i=0; i < NxN; i++){
+        for(int j=0; j< NxN; j++ ){
 
             cout << setw(3);
             cout <<" "<< X[i][j];
-            if (j==N-1){
 
-                cout << endl;
+            if (j==NxN-1){
 
+                cout<<endl;
             }
         }
     }
+*/
 
-    //Verificando si hay Ceros (abajo del 1)
-
-    do{
-        for (int M=a; M<N; M++){
-
-            cout << "< Haciedo Cero a los elementos. Interaccion #>" << M << endl;
-            estatico += b;
-
-            if(X[M][b]!=0){
-
-                //buscando el multiplo
-                //aux = BuscarMultiplo(b,X,M,N);
-
-                for(int L=b; L<N; L++){
-
-                    cout<<"El valor de M es:"<<M<<endl;
-                    cout<<"El valor de estatico es: "<<estatico<<endl;
-                    cout<<"El valor de L es: "<<L<<endl;
-                    cout<<"El valor de X[estatico][L] es: "<<X[estatico][L]<<endl;
-                    cout<<"El valor de -1*X[M][b] es: "<<(-1)*(X[M][b])<<endl;
-                    cout<<"El valor de B es: "<<b<<endl;
-                    eliminar = (X[estatico][L])*(-1*X[M][b]);// 3 * (-1*5)= -15
-
-                    cout<<"El valor de ELIMIAR es: "<<eliminar<<endl;
-                    //eliminar = -1*X[M][b];
-                    X[M][L] = X[M][L] + eliminar; //X = 5+(-5) = 0
-
-                }
-            }
-            b+=1;
-        }
-        a+=1;
-
-        cout << b << endl;
-    }
-    while(a<N);
-
-
-
-    cout << "Imprimiendo la nueva matriz" << endl;
-
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    /** Final del proceso**/
+    cout<<"Imprimiendo la nueva matriz"<<endl;
+    for(int i=0; i < NxN; i++){
+        for(int j=0; j< NxN; j++ ){
 
             cout << setw(3);
-            cout <<" "<<setprecision(1)<< X[i][j];
+            cout <<"|"<<setprecision(6)<< X[i][j];
 
-            if (j==N-1){
+            if (j==NxN-1){
 
-                cout << endl;
+                cout<<endl;
             }
         }
     }
 
     return 0;
+}
+void DiagonalEnUno(int NxN, float X[][100], float diagonal){
+
+    for (int D=1; D<NxN; D++){
+
+        diagonal = X[D][D];
+
+        for (int M=1; M<NxN; M++){
+
+            X[D][M] = (X[D][M])/(diagonal);
+        }
+    }
+}
+void Ingreso_de_datos(float X[][100], float temp[][100], int NxN){
+
+        cout<<"\nDigite los elementos de la matriz en la posicion: "<<endl;
+
+    for(int i=0; i<NxN; i++){
+        for(int j=0; j<NxN; j++){
+
+//            X[i][j] = rand()%4+1;       /* NÃºmeros al azar por pruebas*/
+            cout << "[" << i << "," << j <<"] = ";
+            cin >> X[i][j];               /* Ingreso de datos*/
+            temp[i][j] = X[i][j];         /* Pasar datos a Matriz Auxiliar*/
+        }
+    }
 }
